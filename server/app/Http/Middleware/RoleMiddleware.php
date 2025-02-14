4efc1ba385ab8995e319a46ba7247fadea4777e1
mohammed <mohammedhassouna000@gmail.com>
2025-02-14 23:21:24 +0300

    Add Role middleware for authorization

---
+++ server/app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, ...$roles): Response
+    {
+         // Check if the user is authenticated
+         if (!Auth::check()) {
+            return response()->json(['error' => 'Unauthenticated'], 401);
+        }
+
+        // Check if the user role is in the allowed roles
+        if (!in_array(Auth::user()->role_id, $roles)) {
+            return response()->json(['error' => 'Unauthorized'], 403);
+        }
+
+        return $next($request);
+    }
+}
