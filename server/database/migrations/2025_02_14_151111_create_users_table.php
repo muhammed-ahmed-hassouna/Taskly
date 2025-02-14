dc1df770b622e3d2d993288f5d8d4c6d2b8b04f8
mohammed <mohammedhassouna000@gmail.com>
2025-02-14 19:11:56 +0300

    setup migration files

--- server/database/migrations/2025_02_14_151111_create_users_table.php
+++ server/database/migrations/2025_02_14_151111_create_users_table.php
@@ -13,12 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->index();
+            $table->foreignId('role_id')->default(2)->constrained()->onDelete('cascade')->index();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
