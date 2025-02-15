9fae93befd55f35a4181632dd1425d4d30f8cd5b
mohammed <mohammedhassouna000@gmail.com>
2025-02-15 13:24:19 +0300

    add login & signup query

--- client/src/queries/postQueryFns.js
+++ client/src/queries/postQueryFns.js
@@ -1,2 +1,13 @@
 import api from "../configs/api";
 
+// Sign Up
+export async function signUpUser(formData) {
+  const { data } = await api.post(`/user/register`, { ...formData });
+  return data;
+}
+
+// Log in
+export async function LogInUser(formData) {
+  const { data } = await api.post(`/user/login`, { ...formData });
+  return data;
+}
