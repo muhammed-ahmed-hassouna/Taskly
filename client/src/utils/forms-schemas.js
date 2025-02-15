a9fa8d47fa8900d40cdc2f5672f2435da1f26223
mohammed <mohammedhassouna000@gmail.com>
2025-02-15 13:23:47 +0300

    Add yup validation for login & signup

--- client/src/utils/forms-schemas.js
+++ client/src/utils/forms-schemas.js
@@ -1,2 +1,32 @@
 import * as yup from "yup";
 
+
+export const signUpSchema = yup.object().shape({
+  name: yup
+    .string()
+    .min(3, "Username must be at least 3 characters")
+    .max(50, "Username must be at most 50 characters")
+    .required("Username is required"),
+  email: yup
+    .string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  password: yup
+    .string()
+    .matches(
+      /^(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[\!@#$%^&*()\\[\]{}\-_+=~`|:;"'<>,.?]).{8,}$/,
+      "Invalid password. Password must have 8 characters, with at least 1 number, uppercase, and special character",
+    )
+    .required("Password is required"),
+});
+
+export const loginSchema = yup.object().shape({
+  email: yup.string().email("Invalid email").required("Email is required"),
+  password: yup
+    .string()
+    .required("Password is required")
+    .matches(
+      /^(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[\!@#$%^&*()\\[\]{}\-_+=~`|:;"'<>,.?]).{8,}$/,
+      "Invalid password. Password must have 8 characters, with at least 1 number, uppercase, and special character",
+    ),
+});
