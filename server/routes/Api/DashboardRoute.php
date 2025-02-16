ddf9f02bf23b9bc7d2173b5e235d244a407d6ecf
mohammed <mohammedhassouna000@gmail.com>
2025-02-16 20:53:09 +0300

    add getAllUsers

--- server/routes/Api/DashboardRoute.php
+++ server/routes/Api/DashboardRoute.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum', 'role:1'])->group(function () {
     Route::get('/dashboard/getUsersTask', [DashboardController::class, 'index']);
+    Route::get('/dashboard/getAllUsers', [DashboardController::class, 'getAllUsers']);
     Route::post('/dashboard/assignTask', [DashboardController::class, 'assignTask']);
     Route::patch('/dashboard/updateTask/{id}', [DashboardController::class, 'updateTask']);
     Route::delete('/dashboard/deleteTask/{id}', [DashboardController::class, 'deleteTask']);
