3c131f40f8e8d618af09e28d998c0a6b2c0675bb
mohammed <mohammedhassouna000@gmail.com>
2025-02-16 19:43:53 +0300

    Add database seeder

--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +15,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // Create Admin User
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin2@gmail.com',
+            'password' => Hash::make('Admin123#@'),
+            'role_id' => 1,
+            'email_verified_at' => now(),
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Create Regular User
+        User::create([
+            'name' => 'User One',
+            'email' => 'user1@gmail.com',
+            'password' => Hash::make('User123#@'),
+            'role_id' => 2,
+            'email_verified_at' => now(),
+        ]);
     }
 }
